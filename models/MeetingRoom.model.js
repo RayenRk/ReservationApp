91917137e7e74e11974808241d38118cf54aca1e
RayenRk <rakkadrayen@gmail.com>
2024-03-21 16:58:28 +0100

    🥈 Models

---
+++ models/MeetingRoom.model.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const meetingRoomSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  capacity: { type: Number, required: true },
+  equipments: [{ type: String }],
+  availability: { type: Boolean, default: true },
+});
+
+const MeetingRoom = mongoose.model("MeetingRoom", meetingRoomSchema);
+
+module.exports = MeetingRoom;
---
+++ models/Reservation.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const reservationSchema = new mongoose.Schema({
+    room: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'MeetingRoom',
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    startTime: {
+        type: Date,
+        required: true
+    },
+    endTime: {
+        type: Date,
+        required: true
+    }
+});
+
+const Reservation = mongoose.model('Reservation', reservationSchema);
+
+module.exports = Reservation;
